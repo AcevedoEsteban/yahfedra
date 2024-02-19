7d8fd35553bcc55ab54b47dae1d58cb788cc398a
AcevedoEsteban <astevenaa@gmail.com>
2024-02-19 13:40:27 -0700

    test

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    allowedHeaders: [
+      'Accept',
+      'Authorization',
+      'Content-Type',
+      'X-Requested-With',
+      'apollo-require-preflight',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  });
+
   //edit APP_URL here
   await app.listen(3000);
 }
